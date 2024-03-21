d286c4cacf2b0b5ad39c3be6949a296b91f213f1
Akshansh0812 <akshanshanand2001@gmail.com>
2024-03-22 00:27:43 +0530

    Currency section added

--- app/src/main/java/com/example/bankingappui/MainActivity.kt
+++ app/src/main/java/com/example/bankingappui/MainActivity.kt
@@ -61,7 +61,7 @@ fun HomeScreen() {
             CardsSection()
                 Spacer (modifier = Modifier.height(16.dp))
             FinanceSection()
-//            CurrenciesSection()
+            CurrenciesSection()
         }
     }
 }
